76be2cb9f40c04fa9989895ca6d6af4568e1c984
Denver Baumgartner <denverbaumgartner@gmail.com>
2020-04-24 19:41:09 -0500

    text_grabber
    function to return highlighted text.
    Key commands need to be installed for this to have user functionality.
    
    code from: https://stackoverflow.com/questions/4712310/javascript-how-to-detect-if-a-word-is-highlighted

---
+++ Aspin_Chrome_Extension/textGrabber.js
@@ -0,0 +1,12 @@
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+document.onmouseup = getSelectedText
+document.onkeyup = getSelectedText
